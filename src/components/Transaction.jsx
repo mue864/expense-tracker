767a2c197f65bd25b9321c722d2c90e3a68988ce
mue864 <mumurapa@gmail.com>
2024-12-15 19:19:06 +0200

    fixed errors

---
+++ src/components/Transaction.jsx
@@ -0,0 +1,13 @@
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
+export const Transaction = ({transaction}) => {
+  const {deleteTransaction} = useContext(GlobalContext);
+    const sign = transaction.amount < 0 ? "-" : "+";
+
+  return (
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+      {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
+      <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
+    </li>
+  );
+}
